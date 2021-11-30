820be73bb20225793522e9e7cd92b259ea9fa080
Owen Brush <owenbrush@gmail.com>
2021-11-30 09:23:36 -0800

    wrote code for building time of day features

--- custom_scripts/preparation.py
+++ custom_scripts/preparation.py
@@ -3,6 +3,7 @@ from typing import Sequence
 from pandas.core.arrays import categorical
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import math
 
 RANDOM_STATE = 42
 TEST_SIZE = 0.3
@@ -23,8 +24,13 @@ NUMERIC_FEATURES = [    "fl_num_avg_arr_delay",
                         "fl_num_avg_late_aircraft_delay",
                         "fl_num_avg_total_add_gtime",
                         "fl_num_avg_longest_add_gtime"] 
+
 CATEGORICAL_FEATURES =[]                              
 
+OTHER_FEATURES = ['arr_time_sin',
+                  'arr_time_cos',
+                  'dep_time_sin',
+                  'dep_time_cos']
 
 def get_train_test_split(X:pd.DataFrame, y:pd.Series)-> list:
     """ Returns train_test_split using consntant values for test size and random state"""
@@ -56,8 +62,9 @@ def standardize_data(data_arr:list, scaler,
 
 def build_all_features(flight_data: pd.DataFrame) -> pd.DataFrame:
     """ Returns dataframe with all features added. """
-    flight_data = build_all_features(flight_data.copy())
-    
+    flight_data = build_all_features(flight_data)
+    flight_data = build_time_features(flight_data)
+    return flight_data
 
 
 def build_historic_average_features(flight_data):
@@ -66,5 +73,26 @@ def build_historic_average_features(flight_data):
     return pd.merge(flight_data.copy(), average_delays, on='op_carrier_fl_num')
 
 
-
-
+def build_time_features(flight_data):
+    flight_data = flight_data.copy()
+    ### pad values
+    flight_data['arrival_time'] = flight_data['crs_arr_time'].astype(str).str.zfill(4)
+    flight_data['departure_time'] = flight_data['crs_dep_time'].astype(str).str.zfill(4)
+    ### convert to datetime
+    flight_data['arrival_time'] = pd.to_datetime(flight_data['arrival_time'], format = '%H%M', errors='coerce')
+    flight_data['departure_time'] = pd.to_datetime(flight_data['departure_time'], format = '%H%M', errors='coerce')
+    ### convert to minute of day
+    flight_data['arrival_time'] = flight_data['arrival_time'].dt.hour * 60 + flight_data['arrival_time'].dt.minute
+    flight_data['departure_time'] = flight_data['departure_time'].dt.hour * 60 + flight_data['departure_time'].dt.minute
+    ### normalize with 2*PI
+    flight_data['arrival_time'] = 2 * math.pi * flight_data['arrival_time'] / flight_data['arrival_time'].max()
+    flight_data['departure_time'] = 2 * math.pi * flight_data['departure_time'] / flight_data['departure_time'].max()
+    ### create sin feature
+    flight_data['arr_time_sin'] = np.sin(flight_data['arrival_time'])
+    flight_data['dep_time_sin'] = np.sin(flight_data['departure_time'])
+    ### create cos feature
+    flight_data['arr_time_cos'] = np.cos(flight_data['arrival_time'])
+    flight_data['dep_time_cos'] = np.cos(flight_data['departure_time'])
+    ### drop placeholder features
+    flight_data.drop(['arrival_time','departure_time'],1,inplace=True)
+    return flight_data
