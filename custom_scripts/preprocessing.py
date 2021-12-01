8a8792dabe341d4ca33bfa83ddea2e7546c37d59
Owen Brush <owenbrush@gmail.com>
2021-12-01 08:12:12 -0800

    updated documentation

--- custom_scripts/preprocessing.py
+++ custom_scripts/preprocessing.py
@@ -53,11 +53,11 @@ def get_test_flights(features:str=PRIMARY_TEST_FEATURES) -> pd.DataFrame:
                             """)
 def get_train_flights(features:str=PRIMARY_FEATURES) -> pd.DataFrame:
     """ 
-    Returns DataFrame of all flights from January 2019
+    Returns DataFrame of all flights from first week of January 2019
     
     Accepts an optional argument for specific features to query in string format
     
-        Example: get_2019_january_data("fl_date,tail_num,distance") 
+        Example: get_train_flights("fl_date,tail_num,distance") 
     """
     flights = database.query(f"""SELECT {features}
                              FROM flights
@@ -68,81 +68,3 @@ def get_train_flights(features:str=PRIMARY_FEATURES) -> pd.DataFrame:
     flights = flights[flights['op_carrier_fl_num'].isin(flight_numbers['op_carrier_fl_num'].values)]
     flights = flights[flights['arr_delay'].notnull()]
     return  flights
-
-# def get_all_january_data(features:str=PRIMARY_FEATURES) -> pd.DataFrame:
-#     """ 
-#     Returns DataFrame of all flights across all years from the month of january
-    
-#     Accepts an optional argument for specific features to query in string format
-    
-#         Example: get_all_january_data("fl_date,tail_num,distance") 
-#     """
-#     return database.query(f"""SELECT {features}
-#                              FROM flights
-#                             WHERE SUBSTRING(fl_date,6,2) = '01'
-#                              """)
-    ### Uncertain if data should be filtered at this point ###
-    # #get tables
-    # unique_carriers = pd.read_csv('../data/preprocessing/unique_carriers.csv')
-    # flight_numbers = pd.read_csv('../data/preprocessing/test_flight_numbers.csv')  
-    # #apply filters
-    # flights = flights[flights['op_unique_carrier'].isin(unique_carriers['op_unique_carrier'].values)]
-    # flights = flights[flights['op_carrier_fl_num'].isin(flight_numbers['op_carrier_fl_num'].values)]
-    # flights = flights[flights['arr_delay'].notnull()]
-    # return  flights
-
-# def get_2018_january_data(features:str=PRIMARY_FEATURES) -> pd.DataFrame:
-#     """ 
-#     Returns DataFrame of all flights from January 2018
-    
-#     Accepts an optional argument for specific features to query in string format
-    
-#         Example: get_2018_january_data("fl_date,tail_num,distance") 
-#     """
-#     return database.query(f"""SELECT {features}
-#                              FROM flights
-#                             WHERE SUBSTRING(fl_date,1,7) = '2018-01'
-#                              """)
-
-    
-    
-# def get_1st_last_week(features:str=PRIMARY_FEATURES) -> pd.DataFrame:
-#     """ Returns DataFrame from the last week of the dataset
-#         Accepts an optional argument for specific features to query in string format
-#             Example: get_all_january_data("fl_date,tail_num,distance") """
-#     return database.query(f"""SELECT {features}
-#                              FROM flights
-#                             WHERE fl_date = ANY('{{2019-12-31, 2019-12-30, 2019-12-29, 2019-12-28, 2019-12-27, 2019-12-26, 2019-12-25}}')
-#                              """)  
-# def get_2nd_last_week(features:str=PRIMARY_FEATURES) -> pd.DataFrame:
-#     """ Returns DataFrame from the second last week of the dataset
-#         Accepts an optional argument for specific features to query in string format
-#             Example: get_all_january_data("fl_date,tail_num,distance") """
-#     return database.query(f"""SELECT {features}
-#                              FROM flights
-#                             WHERE fl_date = ANY('{{2019-12-24, 2019-12-23, 2019-12-22, 2019-12-21, 2019-12-20, 2019-12-19, 2019-12-18}}')
-#                              """)
-# def get_3rd_last_week(features:str=PRIMARY_FEATURES) -> pd.DataFrame:
-#     """ Returns DataFrame from the third last week of the dataset
-#         Accepts an optional argument for specific features to query in string format
-#             Example: get_all_january_data("fl_date,tail_num,distance") """
-#     return database.query(f"""SELECT {features}
-#                              FROM flights
-#                             WHERE fl_date = ANY('{{2019-12-17, 2019-12-16, 2019-12-15, 2019-12-14, 2019-12-13, 2019-12-12, 2019-12-11}}')
-#                              """)
-# def get_4th_last_week(features:str=PRIMARY_FEATURES) -> pd.DataFrame:
-#     """ Returns DataFrame from the fourth last week of the dataset
-#         Accepts an optional argument for specific features to query in string format
-#             Example: get_all_january_data("fl_date,tail_num,distance") """
-#     return database.query(f"""SELECT {features}
-#                              FROM flights
-#                             WHERE fl_date = ANY('{{2019-12-10, 2019-12-09, 2019-12-08, 2019-12-07, 2019-12-06, 2019-12-05, 2019-12-04}}')
-#                              """)
-# def get_5th_last_week(features:str=PRIMARY_FEATURES) -> pd.DataFrame:
-#     """ Returns DataFrame from the fifth last week of the dataset
-#         Accepts an optional argument for specific features to query in string format
-#             Example: get_all_january_data("fl_date,tail_num,distance") """
-#     return database.query(f"""SELECT {features}
-#                              FROM flights
-#                             WHERE fl_date = ANY('{{2019-12-03, 2019-12-02, 2019-12-01, 2019-11-30, 2019-11-29, 2019-11-28, 2019-11-27}}')
-#                              """)
